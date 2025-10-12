f4bc22cba82cbc8aaa58b1e3e49f1a889d2003c0
xing <52873782+LZX842056112@users.noreply.github.com>
2025-10-12 14:22:11 +0800

    01

---
+++ 02_PIL中的卷积滤波核参数.py
@@ -0,0 +1,48 @@
+# PyCharm 快捷方式：全部收起 Ctrl + Shift + -
+# 从 pillow 库加载 图像滤波/卷积
+from PIL import ImageFilter
+
+# class BLUR(BuiltinFilter):
+#     name = "Blur"
+#     # fmt: off
+#     filterargs = (5, 5), 16, 0, (
+#         1, 1, 1, 1, 1,
+#         1, 0, 0, 0, 1,
+#         1, 0, 0, 0, 1,
+#         1, 0, 0, 0, 1,
+#         1, 1, 1, 1, 1,
+#     )
+#     # fmt: on
+#
+#
+# class CONTOUR(BuiltinFilter):
+#     name = "Contour"
+#     # fmt: off
+#     filterargs = (3, 3), 1, 255, (
+#         -1, -1, -1,
+#         -1,  8, -1,
+#         -1, -1, -1,
+#     )
+#     # fmt: on
+
+
+# class DETAIL(BuiltinFilter):
+#     name = "Detail"
+#     # fmt: off
+#     filterargs = (3, 3), 6, 0, (
+#         0,  -1,  0,
+#         -1, 10, -1,
+#         0,  -1,  0,
+#     )
+#     # fmt: on
+
+
+# class EMBOSS(BuiltinFilter):
+#     name = "Emboss"
+#     # fmt: off
+#     filterargs = (3, 3), 1, 128, (
+#         -1, 0, 0,
+#         0,  1, 0,
+#         0,  0, 0,
+#     )
+#     # fmt: on
